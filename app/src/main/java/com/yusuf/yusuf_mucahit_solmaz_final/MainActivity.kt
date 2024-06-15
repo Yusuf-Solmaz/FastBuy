3360a3635573ad5e3f61af2f30b391ba7c2e071a
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-15 19:52:23 +0300

    Init Commit

---
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.yusuf.yusuf_mucahit_solmaz_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf.yusuf_mucahit_solmaz_final.ui.theme.Yusuf_Mucahit_Solmaz_FinalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Yusuf_Mucahit_Solmaz_FinalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Yusuf_Mucahit_Solmaz_FinalTheme {
+        Greeting("Android")
+    }
+}
