c0ce50161b9fa1627d569df6885455d299c69a58
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-20 12:48:16 +0300

    Unnecessary Imports Deleted

--- app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/MainActivity.kt
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.yusuf.yusuf_mucahit_solmaz_final
 
-import android.content.Intent
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.yusuf.yusuf_mucahit_solmaz_final.navigation.MainViewModel
 import com.yusuf.yusuf_mucahit_solmaz_final.navigation.Navigation
-import com.yusuf.yusuf_mucahit_solmaz_final.presentation.authPage.SignUpScreen
 import com.yusuf.yusuf_mucahit_solmaz_final.ui.theme.Yusuf_Mucahit_Solmaz_FinalTheme
 import dagger.hilt.android.AndroidEntryPoint
 
