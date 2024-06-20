1b2d13877a10b72c77e3f09e8cbd712b8e9b282b
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-20 12:44:36 +0300

    Login and GetCurrentUser Added

--- app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/di/AppModule.kt
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/di/AppModule.kt
@@ -1,20 +1,25 @@
 package com.yusuf.yusuf_mucahit_solmaz_final.di
 
 import android.content.Context
+import com.yusuf.yusuf_mucahit_solmaz_final.data.datastore.SessionManager
+import com.yusuf.yusuf_mucahit_solmaz_final.data.datastore.repo.UserSessionRepository
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.network.ApiService
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.addToCart.AddToCartUseCase
+import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getCurrentUser.GetCurrentUserUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getProductById.GetProductByIdUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getProductUseCase.GetProductUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getProductsByCategory.GetProductsByCategoryUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getProductsCategory.GetProductsCategoryUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getUserCart.GetUserCartUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getUserProfile.GetUserProfileUseCase
+import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.login.LoginUseCase
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.updateUserProfile.UpdateUserProfileUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
@@ -61,6 +66,16 @@ object AppModule {
     @Provides
     fun updateProfile(apiService: ApiService): UpdateUserProfileUseCase {
         return UpdateUserProfileUseCase(apiService)
-
     }
+
+    @Provides
+    fun login(apiService: ApiService): LoginUseCase {
+        return LoginUseCase(apiService)
+    }
+
+    @Provides
+    fun getCurrentUser(apiService: ApiService): GetCurrentUserUseCase {
+        return GetCurrentUserUseCase(apiService)
+    }
+
 }
