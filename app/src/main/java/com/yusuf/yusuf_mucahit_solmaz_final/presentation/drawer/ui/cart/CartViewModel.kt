8d7ce0e5de468bfc0647f5b6638fc1e22eebf8d8
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-20 12:59:17 +0300

    UserSessionRepository Injected

--- app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/presentation/drawer/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/presentation/drawer/ui/cart/CartViewModel.kt
@@ -1,28 +1,40 @@
 package com.yusuf.yusuf_mucahit_solmaz_final.presentation.drawer.ui.cart
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusuf.yusuf_mucahit_solmaz_final.core.AppResult.GeneralResult
+import com.yusuf.yusuf_mucahit_solmaz_final.data.datastore.SessionManager
+import com.yusuf.yusuf_mucahit_solmaz_final.data.datastore.repo.UserSessionRepository
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.useCase.getUserCart.GetUserCartUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CartViewModel @Inject constructor(
     private val getUserCartUseCase: GetUserCartUseCase,
+    private val session: UserSessionRepository
 ) : ViewModel() {
 
     private val _cart = MutableLiveData<CartState>()
     val cart: LiveData<CartState> = _cart
 
+
+    init{
+        Log.d("session", "init: ${session.getUserId()}")
+    }
+
+
     fun getUserCart() {
         _cart.value = CartState(isLoading = true)
+
         viewModelScope.launch {
-            getUserCartUseCase.getUserCardById().collect { result ->
+            getUserCartUseCase.getUserCardById(session.getUserId()).collect { result ->
                 when(result){
                     is GeneralResult.Error -> {
                         _cart.postValue(CartState(
