a2386aa497857f8341014cc892fb2ed86ef26127
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-24 00:33:48 +0300

    SearchAddToCartState Created

---
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/presentation/drawer/ui/search/state/SearchAddToCartState.kt
@@ -0,0 +1,7 @@
+package com.yusuf.yusuf_mucahit_solmaz_final.presentation.drawer.ui.search.state
+
+data class SearchAddToCartState(
+    val isLoading: Boolean = false,
+    val success: Boolean? = null,
+    val error: String? = null
+)
