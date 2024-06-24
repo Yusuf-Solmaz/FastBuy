cb16302ef07c6054f48ada3201767e575683cda2
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-24 14:06:55 +0300

    Error & Loading Handling Update

--- app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/presentation/drawer/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yusuf/yusuf_mucahit_solmaz_final/presentation/drawer/ui/profile/ProfileFragment.kt
@@ -13,6 +13,9 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.yusuf.yusuf_mucahit_solmaz_final.R
+import com.yusuf.yusuf_mucahit_solmaz_final.core.utils.ViewUtils.gone
+import com.yusuf.yusuf_mucahit_solmaz_final.core.utils.ViewUtils.setVisibility
+import com.yusuf.yusuf_mucahit_solmaz_final.core.utils.ViewUtils.visible
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remote.responses.profile.UpdateUserProfileRequest
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remoteconfig.RemoteConfigManager.loadBackgroundColor
 import com.yusuf.yusuf_mucahit_solmaz_final.data.remoteconfig.RemoteConfigManager.updateUI
@@ -24,9 +27,9 @@ class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private val viewModel: ProfileViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -40,61 +43,64 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loadBackgroundColor(requireContext()){
-                color->
+        loadBackgroundColor(requireContext()) { color ->
             view.setBackgroundColor(Color.parseColor(color))
         }
 
         viewModel.getUserProfile()
 
+
+
         viewModel.profile.observe(viewLifecycleOwner) { state ->
-            when {
-                state.isLoading -> {
 
-                }
+            setVisibility(
+                isLoading = state.isLoading,
+                isError = state.error != null,
+                isSuccess = state.profileResponse != null,
+                loadingView = binding.profileLoadingErrorComponent.loadingLayout,
+                errorView = binding.profileLoadingErrorComponent.errorLayout,
+                successView = binding.profileLayout
+            )
 
-                state.error != null -> {
+            if (state.profileResponse != null) {
+                binding.name.text = Editable.Factory.getInstance().newEditable(state.profileResponse.firstName)
+                binding.lastName.text = Editable.Factory.getInstance().newEditable(state.profileResponse.lastName)
+                binding.email.text = Editable.Factory.getInstance().newEditable(state.profileResponse.email)
+                binding.phoneNumber.text = Editable.Factory.getInstance().newEditable(state.profileResponse.phone)
+                binding.password.text = Editable.Factory.getInstance().newEditable(state.profileResponse.password)
 
-                }
-
-                state.profileResponse != null -> {
-
-                    binding.loadingAnimation.visibility = View.VISIBLE
-                    binding.name.text = Editable.Factory.getInstance().newEditable(state.profileResponse.firstName)
-                    binding.lastName.text = Editable.Factory.getInstance().newEditable(state.profileResponse.lastName)
-                    binding.email.text = Editable.Factory.getInstance().newEditable(state.profileResponse.email)
-                    binding.phoneNumber.text = Editable.Factory.getInstance().newEditable(state.profileResponse.phone)
-                    binding.password.text = Editable.Factory.getInstance().newEditable(state.profileResponse.password)
-
-                    binding.updateBtn.setOnClickListener {
-                        val request = UpdateUserProfileRequest(
-                            name = binding.name.text.toString(),
-                            lastName = binding.lastName.text.toString(),
-                            email = binding.email.text.toString(),
-                            phone = binding.phoneNumber.text.toString(),
-                            password = binding.password.text.toString()
-                        )
-                        viewModel.updateUserProfile(request)
-                    }
-                }
+                binding.updateBtn.setOnClickListener(::updateUserInformation)
             }
         }
 
-        viewModel.updateProfile.observe(viewLifecycleOwner, Observer { state ->
-            when {
-                state.isLoading -> {
+        viewModel.updateProfile.observe(viewLifecycleOwner) { state ->
 
-                }
-                state.error != null -> {
+            setVisibility(
+                isLoading = state.isLoading,
+                isError = state.error != null,
+                isSuccess = state.success != null,
+                loadingView = binding.profileLoadingErrorComponent.loadingLayout,
+                errorView = binding.profileLoadingErrorComponent.errorLayout,
+                successView = binding.profileLayout
+            )
 
-                }
-                state.success != null -> {
-                    Toast.makeText(requireContext(), "Profile updated", Toast.LENGTH_SHORT).show()
-                    val action =  ProfileFragmentDirections.actionNavProfileToNavHome()
-                    findNavController().navigate(action)
-                }
-
+            if (state.success != null) {
+                Toast.makeText(requireContext(), "Profile updated", Toast.LENGTH_SHORT).show()
+                val action = ProfileFragmentDirections.actionNavProfileToNavHome()
+                findNavController().navigate(action)
             }
-        })
+        }
+    }
+
+    private fun updateUserInformation(view: View){
+
+        val request = UpdateUserProfileRequest(
+            name = binding.name.text.toString(),
+            lastName = binding.lastName.text.toString(),
+            email = binding.email.text.toString(),
+            phone = binding.phoneNumber.text.toString(),
+            password = binding.password.text.toString()
+        )
+        viewModel.updateUserProfile(request)
     }
 }
